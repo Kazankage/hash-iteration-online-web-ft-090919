6ca1d14c5cdae9453fd27051e38c8db2e195f500
Randel Jiao <randeljiao@gmail.com>
2019-10-05 05:15:41 +0000

    Automatically backed up by Learn

--- ger.rb
+++ ger.rb
@@ -18,3 +18,13 @@ end
 
 #select_winner(passengers)
 
+birthday_kids = {
+  "Timmy" => 9,
+  "Sarah" => 6,
+  "Amanda" => 27
+}
+
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age| 
+    puts ""
+end
